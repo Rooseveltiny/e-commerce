15d070321adc6b646b8d46503fb11ce53627f347
Rooseveltiny <mister.savvvik@gmail.com>
2019-10-22 12:26:58 +0300

    added many new func

--- db_tutorial/db_orm/views.py
+++ db_tutorial/db_orm/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from .push_xml import push_all
 from .models import Product
 from django.core.paginator import Paginator
+from uuid import UUID
+from django.db.models import Q
 
 # Create your views here
 
@@ -24,8 +26,7 @@ def catalog(request):
 
     products_list = Product.objects.all()
 
-    filter_params = request.POST.getlist('filter_items')
-
+   
     # block that forms group of features_groups with features just to make a filter
     all_features = set()
     for product in products_list:
@@ -40,22 +41,46 @@ def catalog(request):
     for group in feature_groups:
 
         features_of_group = []
-
         for feature in all_features:
             if feature.feature_group == group:
 
                 features_of_group.append(feature)
+            
 
         group_element = {'name': group.name,
-                         'features': features_of_group}
+                         'features': features_of_group,
+                         } 
 
         groups_with_features.append(group_element)
     # end block
 
-    products_list = products_list.filter(features_link__in = filter_params)
-    products_list = list(dict.fromkeys(products_list))
+    
+    filter_params = []
+    if request.method == 'POST':
 
+        
+        filter_params = request.POST.getlist('filter_items')
+
+        if len(filter_params):
+
+            # if len(filter_params) > 0:
+
+            #     q_objects = Q()
+
+            #     for item in filter_params:
+            #         q_objects.add(Q(pk=item), Q.AND)
+
+            #     products_list = products_list.filter(q_objects)        
+
+            products_list = products_list.filter(features_link__in = filter_params)
+            products_list = list(dict.fromkeys(products_list))
+    
     context = {'products': products_list,
-               'groups_with_features': groups_with_features}
+               'groups_with_features': groups_with_features,
+               'filter_params': [UUID(x) for x in filter_params],
+               }
+
+               
+
 
     return render(request, 'catalog.html', context=context)
