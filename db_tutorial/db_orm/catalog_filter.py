041a39a2a775a6fac92b7198e9607bcbc69c8b65
Rooseveltiny <mister.savvvik@gmail.com>
2019-10-22 17:38:35 +0300

    refactoring filter

---
+++ db_tutorial/db_orm/catalog_filter.py
@@ -0,0 +1,55 @@
+from .models import Feature
+
+def get_groups_with_features(products_list):
+
+    all_features = set()
+    for product in products_list:
+        for feature in product.features_link.all():
+            all_features.add(feature)
+
+    feature_groups = set()
+    for feature in all_features:
+        feature_groups.add(feature.feature_group)
+
+    groups_with_features = []
+    for group in feature_groups:
+
+        features_of_group = []
+        for feature in all_features:
+            if feature.feature_group == group:
+
+                features_of_group.append(feature)
+
+        group_element = {'name': group.name,
+                         'features': features_of_group,
+                         }
+
+        groups_with_features.append(group_element)
+
+    return groups_with_features
+
+def filter_products(filter_params, products_list):
+
+    all_features = Feature.objects.filter(link__in=filter_params)
+    all_groups = set([x.feature_group for x in all_features])
+
+    groups_with_features = []
+    for group in all_groups:
+
+        collected_features_for_group = []
+        for feature in all_features:
+            if feature.feature_group == group:
+                collected_features_for_group.append(feature)
+
+        groups_with_features.append(
+                    {
+                        'name': group.name,
+                        'features': collected_features_for_group,
+                    }
+                )
+
+    for element in groups_with_features:
+        products_list = products_list.filter(
+            features_link__in=element['features'])
+
+    return products_list
--- db_tutorial/db_orm/views.py
+++ db_tutorial/db_orm/views.py
@@ -5,6 +5,7 @@ from .models import Product, Feature
 from django.core.paginator import Paginator
 from uuid import UUID
 from django.db.models import Q
+from .catalog_filter import get_groups_with_features, filter_products
 
 # Create your views here
 
@@ -27,30 +28,7 @@ def catalog(request):
     products_list = Product.objects.all()
 
     # block that forms group of features_groups with features just to make a filter
-    all_features = set()
-    for product in products_list:
-        for feature in product.features_link.all():
-            all_features.add(feature)
-
-    feature_groups = set()
-    for feature in all_features:
-        feature_groups.add(feature.feature_group)
-
-    groups_with_features = []
-    for group in feature_groups:
-
-        features_of_group = []
-        for feature in all_features:
-            if feature.feature_group == group:
-
-                features_of_group.append(feature)
-
-        group_element = {'name': group.name,
-                         'features': features_of_group,
-                         }
-
-        groups_with_features.append(group_element)
-    # end block
+    groups_with_features = get_groups_with_features(products_list)
 
     filter_params = []
     if request.method == 'POST':
@@ -60,31 +38,7 @@ def catalog(request):
 
         if len(filter_params):
 
-            # collecting all features structured by group
-            all_features = Feature.objects.filter(link__in=filter_params)
-            all_groups = set([x.feature_group for x in all_features])
-
-            groups_with_features1 = []
-            for group in all_groups:
-
-                collected_features_for_group = []
-                for feature in all_features:
-                    if feature.feature_group == group:
-                        collected_features_for_group.append(feature)
-
-                groups_with_features1.append(
-                    {
-                        'name': group.name,
-                        'features': collected_features_for_group,
-                    }
-                )
-            # end block
-
-            for element in groups_with_features1:
-                products_list = products_list.filter(
-                    features_link__in=element['features'])
-
-            # products_list = list(dict.fromkeys(products_list))
+            products_list = filter_products(filter_params, products_list)
 
     context = {'products': products_list,
                'groups_with_features': groups_with_features,
