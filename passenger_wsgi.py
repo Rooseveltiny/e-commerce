97a3cef9629826696e6d952a5625e45f0b026ac8
Rooseveltiny <mister.savvvik@gmail.com>
2019-10-26 18:36:34 +0300

    changed htaccess

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os, sys
 sys.path.append('/home/r/roosevelt/keklol.ru/e-commerce')
-sys.path.append('/home/r/roosevelt/keklol.ru/e-commerce/env/lib/python3.6/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'HelloDjango.settings'
+sys.path.append('./env/lib/python3.6/site-packages')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'db_tutorial.settings'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
